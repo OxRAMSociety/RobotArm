71c7291d88048ce5ec7a0a436971816b05332d15
JacquesCloete <jacques.cloete@trinity.ox.ac.uk>
2022-02-20 10:15:10 +0000

    Added Smach example scripts and files, and STL files for chess pieces

---
+++ Smach/executive_smach_tutorials/examples/state_machine.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Description:
+    Create a simple 3-state state machine.
+
+Usage:
+    $> ./state_machine.py
+
+Output:
+    [INFO] : State machine starting in initial state 'FOO' with userdata: 
+            []
+    [INFO] : State machine transitioning 'FOO':'done'-->'BAR'
+    [INFO] : State machine transitioning 'BAR':'done'-->'BAZ'
+    [INFO] : State machine terminating 'BAZ':'done':'succeeded'
+
+"""
+
+import smach
+
+class ExampleState(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes = ['done'])
+    def execute(self, ud):
+        return 'done'
+
+def main():
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['succeeded','aborted'])
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('FOO', ExampleState(), {'done':'BAR'})
+        smach.StateMachine.add('BAR', ExampleState(), {'done':'BAZ'})
+        smach.StateMachine.add('BAZ',
+                               ExampleState(),
+                               {'done':'succeeded'})
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+if __name__ == '__main__':
+    main()
